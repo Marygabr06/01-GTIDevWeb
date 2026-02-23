214ba11b4eef7886cf168870bdab96eb3f86204f
Marygabr06 <maya.gabriela06@gmail.com>
2026-02-23 17:59:39 -0300

    Item 10 - Implementação de alerta em JavaScript.

--- scripts.js
+++ scripts.js
@@ -1,2 +1,8 @@
-// De propósito, este arquivo começa “quieto”.
-// O desafio pede uma alteração simples aqui.
+// Seleciona o botão pela classe e adiciona o evento de clique
+const botao = document.querySelector('.btn-ghost');
+
+if (botao) {
+    botao.addEventListener('click', function() {
+        alert("Parabéns! Você concluiu o desafio!");
+    });
+}
